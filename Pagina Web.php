e8514c90214678ec21ba5f794dba293c48ca104c
carlcald <carloscalderon1994@hotmail.com>
2014-04-13 09:16:25 -0500

    Codigo

---
+++ Pagina Web.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head> <!-- etiqueta head sirve para representar datos del documento-->
+<meta charset="utf-8"/> <!-- esto sirve para los caracteres o algo asi ._. -->
+<title>Pagina Web HTML5</title> <!--Titulo xD -->
+<link rel="stylesheet" href="estilo1.css"/> <!-- esto es para darle un poco mas de estilo ._. aunque soy novato :c -->
+</head>
+<body>
+
+    <div id="web"> <!-- representa un contenedor-->
+    <header> <!--Define la cabecera de la pagina o seccion-->
+    <hgroup> <!-- la etiqueta hgroup es usada para agrugar titulos ._. y subtitulos-->
+                <h1>AVINYO SA.</h1> <!--TITULO-->
+                <h2>CULTURE DIGITAL</h2>    <!--SUBTITULO-->
+    </hgroup>
+    </header>
+    
+    <nav> <!-- Define una seccion que solamente contiene enlaces de navegacion-->
+    
+        <ul> <!--define una lista sin orden -->
+    
+        <li>Inicio</li>
+        <li>Conocenos</li>
+        <li>Innovacion</li>
+        <li>Actualidad</li>
+        <li>Contactanos</li>
+        
+        </ul>
+    
+    </nav>
+    
+        <div id="main"> <!--abrimos un div y lo llamamos main-->
+     
+        <div id="contenido">
+    
+        <section> <!-- esto es conocido como una seccion de la pagina-->
+    
+        Innovacion
+    
+        <article> <!--como su misma palabra lo dice es un articulo (( 1 )) -->
+        <header><h3>Oovoo</h3></header> <!-- definir la cabecera del articulo-->
+        
+        <div>
+        <p> Conectate con tus amigos mediante llamadas de video y mensajes de texto, a traves de telefonos y tabletas iPhone, iPad y Android, realiza videoconferencias con hasta doce usuarios de un modo simultaneo</p>
+        <!-- sirve para definir un parrafo -->
+        </div>
+    
+        <footer></footer> <!-- pie de pagina del articulo -->
+        </article> <!-- cerramos el articulo-->
+        <hr /> <!-- se utiliza como una linea para separar los articulos-->
+         
+        <article> <!-- creamos otro articulo-->
+        <header><h3>Shazam</h3></header>
+       
+        
+        <div>
+        <p> Si escuchas una cancion y te encanta, o te recuerda a alguien, o piensas que es horrible, pero no sabes el titulo, ni el artista, ni el disco y en ese momento no sabes... que hacer? pero sabes existe una manera instantanea de conocer todos esos datos</p>
+        </div>
+        
+        <footer></footer>
+        </article>
+        </section> <!-- aqui termina la seccion-->
+        
+        <section> <!-- aqui creamos otra seccion-->
+        Conocenos
+        
+        <article>
+        <header><h3>La tecnologia... el cerebro mas inteligente de la especie humana</h3></header>
+        
+        <div>
+        <p>Los estudiantes no son los mismos y sus habitos culturales han cambiado. La cultura digital esta transformando los modos de lectura y escritura al punto que no es suficiente leer y escribir para estar alfabetizado... Habilita la escuela oportunidades para que los estudiantes produzcan conocimiento, sean autonomos, discriminen fuentes confiables, trabajen con otros o usen la informacion critica y eticamente? La escuela tradicional no fue pensada para enseñar a ser ciudadano de la Sociedad del Conocimiento sin sentirse naufrago</p>
+        </div>
+        <footer>    </footer> <!-- pie de pagina para por gusto u.u -->
+        </article>
+        </section>
+    </div>
+    
+    <aside> <!-- sirve para definir algunos contenidos pocos relacionados con la pagina ._. -->
+    
+    <section>
+    <h3>Redes Sociales</h3>
+    <a title="Redes Sociales" href="https://www.facebook.com/carlosantonio.calderonmontes"> <img width="120px" alt="Redes Sociales" src="images\RedesSociales.jpg"/> </a>
+    <!-- representa un hyper enlace como en mi caso seria el de mi perfil de face (( si ven esto agreguen xD ._. )) -->
+    </section>
+    
+    <section>
+    <h3>Publicidad</h3>
+    <img width="120px" alt="Publicidad" src="images\Publicidad.jpg"/>
+    <!-- etiqueta img es para insertar una imagen ._. --> 
+    </section>
+    </aside>
+    </div>
+    
+    <footer>
+    <span>Ejercicio By CarlCald :c PD: no se hacer videos xD :c </span>
+    <!-- sirve para representar un texto sin un significado especifico-->
+    </footer>
+    </div>
+</body> 
+</html>
+
+<!-- ultimo comentario fue todoo.!!!!!!!! :D -->
